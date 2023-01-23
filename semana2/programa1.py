a62dd1af3a2033121eb77feb140cc1c6fd2b07dc
JoelOrtega12 <122801180+JoelOrtega12@users.noreply.github.com>
2023-01-23 14:53:58 +0000

    Se actualizo la tabla de README.md

---
+++ semana2/programa1.py
@@ -0,0 +1,12 @@
+"""
+    programa1
+    Nombre: Joel Arturo Licona Ortega
+    Fecha: 23/01/2023
+    Descripción: Comentarios 
+    Multilinea, Comentarios de 
+    una linea, concatenación de una o 
+    más cadenas de texto
+"""
+variable1 = "  Hola" # variable para almacenar una de cadena de caracteres
+variable2 = "  mundo python" # variable para almacenar una cadena de caracteres
+print(variable1 + variable2)
