016ae434ffa3ef387284c7bcc4da3c5fcebc6a92
JoelOrtega12 <122801180+JoelOrtega12@users.noreply.github.com>
2023-02-07 14:18:49 +0000

    Se actualizo el programa

--- semana3/programa6.py
+++ semana3/programa6.py
@@ -1,9 +1,12 @@
 # Probelma comparar 2 Números enteros e imprimir el número mayor 
 
-print("Ingresar dos Números: ")
-n1 =int(input("Número 1:"))
-n2 =int(input("Número 2:"))
-if n1==n2
-    print("Los 2 Números son iguales")
-elif n1>n2
-    
+n1 = int(input("n1"))
+n2 = int(input("n2"))
+if n1> n2:
+   print (n1)
+if n2> n1:
+   print(n2)
+if n1==n2:
+   print(None)
+
+
